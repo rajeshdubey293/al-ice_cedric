b872399e355a8eb42c84f6c44c75f6cbdbcc3b39
Ryan Andri <ryanandri@linuxmail.org>
2018-01-09 22:28:25 +0700

    cpufreq: santoni: add properly CPU voltage control.
    backport from Kenzo.
    
    Authored billchen1977 on kenzo.
    credits to psndna88.
    
    * sometime voltage control not show up on Kernel adiutor (KA).
    * just simple fix this by open KA > 'cpu' and then 'cpu voltage'.
    * if stil not showing do this continuesly ( cpu > cpu voltage > cpu > cpu voltage or exit KA and then open again)
    * until cpu voltage showing the voltages table. (Tested by me (Ryan Andri)).
    
    - NB: voltage of max frequency can't be changes.
    
    ported from : https://github.com/psndna88/AGNi_pureMIUI/commit/8bd2c45d290a64b51deb4b29672fc30a4eee8149#diff-3e5f3ed85dbd21a175dbe49aaeb62473
    
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -751,6 +751,22 @@ static ssize_t show_bios_limit(struct cpufreq_policy *policy, char *buf)
 	return sprintf(buf, "%u\n", policy->cpuinfo.max_freq);
 }
 
+/* SysFS Undervolts Control */
+extern ssize_t cpu_clock_get_vdd(char *buf);
+extern ssize_t cpu_clock_set_vdd(const char *buf, size_t count);
+
+static ssize_t show_UV_mV_table(struct cpufreq_policy *policy, char *buf)
+{
+	return cpu_clock_get_vdd(buf);
+}
+
+static ssize_t store_UV_mV_table(struct cpufreq_policy *policy,
+	const char *buf, size_t count)
+{
+	return cpu_clock_set_vdd(buf, count);
+}
+/* SysFS Undervolts Control */
+
 cpufreq_freq_attr_ro_perm(cpuinfo_cur_freq, 0400);
 cpufreq_freq_attr_ro(cpuinfo_min_freq);
 cpufreq_freq_attr_ro(cpuinfo_max_freq);
@@ -765,6 +781,9 @@ cpufreq_freq_attr_rw(scaling_min_freq);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
+/* SysFS Undervolts Control */
+cpufreq_freq_attr_rw(UV_mV_table);
+/* SysFS Undervolts Control */
 
 static struct attribute *default_attrs[] = {
 	&cpuinfo_min_freq.attr,
@@ -778,6 +797,9 @@ static struct attribute *default_attrs[] = {
 	&scaling_driver.attr,
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
+	/* SysFS Undervolts Control */
+	&UV_mV_table.attr,
+	/* SysFS Undervolts Control */
 	NULL
 };
 
